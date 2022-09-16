083cb7f36dcd49d08314ca754076b2a52be51b75
yaphott <londowski@proton.me>
2022-09-16 16:04:06 -0500

    Check if version number fetched from releases appears mostly valid

--- bin/install_firacode_font.sh
+++ bin/install_firacode_font.sh
@@ -6,19 +6,55 @@ if [[ ! $INSIDE_SCRIPT ]]; then
 fi
 
 # Install FiraCode Font
-echo 'Installing FiraCode Font'
+echo '+++ Installing FiraCode Font'
 
-# Fetch the latest version number
-# https://fabianlee.org/2021/02/16/bash-determining-latest-github-release-tag-and-version/
 github_user='tonsky'
 github_project='FiraCode'
-latest_release_version=$( curl -s 'https://api.github.com/repos/'"$github_user"'/'"$github_project"'/releases/latest' | jq -r ".tag_name" )
+github_api_url='https://api.github.com/repos/'"$github_user"'/'"$github_project"
+github_std_url='https://github.com/'"$github_user"'/'"$github_project"
 
-# Fetch the latest release
-latest_release_name='Fira_Code_v'"$latest_release_version"
-wget 'https://github.com/tonsky/FiraCode/releases/download/'"$latest_release_version"'/'"$latest_release_name"'.zip' \
-     -O './tmp/'"$latest_release_name"'.zip'
+# Fetch the version of the latest release
+#   https://fabianlee.org/2021/02/16/bash-determining-latest-github-release-tag-and-version/
+latest_release_ver=$( curl -s "$github_api_url"'/releases/latest' | jq -r ".tag_name" )
 
+# Validate release version
+#   Allows for unlimited number/period combinations, and unlimited letters at the end
+#   Must have at least one number in the version
+#   Accepts:     Rejects:
+#     1.2.3        1a.2.3
+#     1.23.3.4a    1.2b.3
+#     1.2.3.4ab    1.2a.3c
+is_valid_release_ver=$( python3 -c '\
+from string import ascii_letters; \
+to_check="'"$latest_release_ver"'"; \
+is_valid=to_check.rstrip(ascii_letters).replace(".","").isdigit(); \
+is_valid_str=str(is_valid).lower(); \
+print(is_valid_str)\
+' )
+if [ ! $is_valid_release_ver = true ]; then
+    echo 'Failed to fetch version for latest release of '"$github_project"' by '"$github_user"'. Exiting ...'
+    exit
+fi
+
+# Download a ZIP file containing the latest release
+latest_release_name='Fira_Code_v'"$latest_release_ver"
+latest_release_file="$latest_release_name"'.zip'
+latest_release_file_url="$github_std_url"'/releases/download/'"$latest_release_ver"'/'"$latest_release_file"
+wget "$latest_release_file_url" -O './tmp/'"$latest_release_file"
+
+# Verify download
+if [[ ! -f './tmp/'"$latest_release_file" ]]; then
+    echo 'Failed to download '"$github_project"' by '"$github_user"' from '"$latest_release_file_url"'. Exiting ...'
+    exit
+fi
+
+# Delete output folder if already exists
+if [[ -d './tmp/'"$latest_release_name" ]]; then
+    echo 'Folder already exists: ./tmp/'"$latest_release_name"'. Deleting ...'
+    rm -r './tmp/'"$latest_release_name"
+fi
+
+# Create output folder and extract
 mkdir './tmp/'"$latest_release_name"
 unzip './tmp/'"$latest_release_name"'.zip' -d './tmp/'"$latest_release_name"
 
@@ -28,7 +64,7 @@ if [[ ! -d "$HOME"'/.fonts' ]]; then
 fi
 
 # Sometimes the uuid file needs deleting and regenerating
-# so we'll do it either way if it exists
+#   So we'll do it either way if it exists
 if [[ -f "$HOME"'/.fonts/uuid' ]]; then
     rm "$HOME"'/.fonts/uuid'
 fi
