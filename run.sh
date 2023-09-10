4f7c91c6134839889a0aca82c1d716d3190d66fd
yaphott <londowski@proton.me>
2023-09-10 04:59:47 -0500

    Change flow to use task registering

--- run.sh
+++ run.sh
@@ -59,15 +59,6 @@ if [[ ! -f './tmp/waypoint.temp' ]]; then ##########################
     sudo shutdown -r now
 fi
 
-# Update package database and install required packages
-#   linux-generic - building software.
-#   build-essential - APT with HTTPS.
-#   apt-transport-https - fetching files and installing keys.
-#   gpg, wget, and curl - release codename.
-bash ./bin/install_dependencies.sh || prompt_to_exit 1
-
-# Install General Packages
-bash ./bin/install_general.sh || prompt_to_exit 1
 
 # User-defined variables (if not set prior to executing)
 while [ ! "$LIVEPATCH_KEY" ]; do
@@ -85,151 +76,44 @@ echo 'Using swapfile size: '"$SWAPFILE_SIZE"
 SWAPFILE_SWAPINESS='10'
 echo 'Using system swappiness: '"$SWAPFILE_SWAPINESS"
 
-# function run_script () {
-#     bash "$1" || prompt_to_exit "$2"
-# }
-# 
-# 
-# tasks=()
-# function add_task () {
-#     # Length is the next index, since index starts at 0
-#     TASK_NAMES[${#TASK_NAMES[@]}]="$1"
-#     TASK_TYPES[${#TASK_TYPES[@]}]="$2"
-#     TASK_CMDS[${#TASK_CMDS[@]}]="$3"
-# }
-# 
-# function execute_tasks () {
-#     for i in "${!TASK_NAMES[@]}"; do
-#         task_label='[ '"${TASK_NAMES[$i]}"' : '"${TASK_NAMES[$i]}"' ]'
-#         echo 'Starting '"$task_label"
-#         # Run the task and prompt to exit if it fails
-#         ( ${TASK_CMDS[$i]} \
-#           && echo 'Finished [ '"${TASK_NAMES[$i]}"' : '"${TASK_NAMES[$i]}"' ]' \
-#         ) || ( \
-#           echo 'Failed [ '"${TASK_NAMES[$i]}"' : '"${TASK_NAMES[$i]}"' ]' \
-#           && prompt_to_exit 1 \
-#         )
-#     done
-# }
-# function unregister_tasks () {
-#     unset TASK_NAMES
-#     unset TASK_TYPES
-#     unset TASK_CMDS
-#     declare -a TASK_NAMES
-#     declare -a TASK_TYPES
-#     declare -a TASK_CMDS
-# }
-# function testit () {
-#     echo 'THIS IS THE TEST'
-# }
-# unregister_tasks
-# add_task 'HTop' 'install' 'testit'
-# execute_tasks
+function reset_tasks () {
+    unset TASK_NAMES
+    unset TASK_TYPES
+    unset TASK_CMDS
+    unset TASK_SHOULD_EXIT
+    declare -a TASK_NAMES
+    declare -a TASK_TYPES
+    declare -a TASK_CMDS
+    declare -a TASK_SHOULD_EXIT
+}
+function register_task () {
+    # Length is the next index, since index starts at 0
+    TASK_NAMES[${#TASK_NAMES[@]}]="$1"
+    TASK_TYPES[${#TASK_TYPES[@]}]="$2"
+    TASK_CMDS[${#TASK_CMDS[@]}]="$3"
+    TASK_SHOULD_EXIT[${#TASK_SHOULD_EXIT[@]}]="$4"
+}
 
-# Configure HTop
-bash ./bin/configure_htop.sh
+function execute_tasks () {
+    for i in "${!TASK_NAMES[@]}"; do
+        task_label='[ '"${TASK_NAMES[$i]}"' : '"${TASK_NAMES[$i]}"' ]'
+        echo 'Starting '"$task_label"' ('"${TASK_TYPES[$i]}"')'
+        echo '... with command: '"${TASK_CMDS[$i]}"
+        # Run the task and prompt to exit if it fails
+        ( eval ${TASK_CMDS[$i]} \
+              && echo 'Finished [ '"${TASK_NAMES[$i]}"' : '"${TASK_NAMES[$i]}"' ]' \
+        ) || ( \
+          echo 'Failed [ '"${TASK_NAMES[$i]}"' : '"${TASK_NAMES[$i]}"' ]' \
+              && ( \
+                  if [[ "${TASK_SHOULD_EXIT[$i]}" == '1' ]]; then \
+                      prompt_to_exit 1; \
+                  fi \
+              ) \
+        )
+    done
+}
 
-# Install Latest Python
-bash ./bin/install_python3.sh || prompt_to_exit 1
-# Configure Latest Python
-bash ./bin/configure_python3.sh || prompt_to_exit 1
-
-# TODO: Install Python 3.9
-# bash ./bin/install_python39.sh || prompt_to_exit 1
-# TODO: Configure Python 3.9
-# bash ./bin/configure_python39.sh || prompt_to_exit 1
-
-# TODO: Install CmdStan
-#       ~/.cmdstan/cmdstan-2.29.1
-# bash ./bin/install_cmdstan.sh || prompt_to_exit 1
-
-# TODO: Install Mujoco
-#       ~/.mujoco/mujoco210
-# bash ./bin/install_mujoco.sh || prompt_to_exit 1
-
-# TODO: Configure Streamlit (e.g. disable watchdog)
-#       ~/.streamlit/config.toml
-# bash ./bin/configure_streamlit || prompt_to_exit 1
-
-# Install Node.js
-bash ./bin/install_nodejs.sh || prompt_to_exit 1
-
-# Install Yarn
-bash ./bin/install_yarn.sh || prompt_to_exit 1
-
-# Install FiraCode Font
-bash ./bin/install_firacode_font.sh || prompt_to_exit 1
-
-# Install Sublime Text
-bash ./bin/install_sublime_text.sh || prompt_to_exit 1
-
-# Install Visual Studio Code
-bash ./bin/install_visual_studio_code.sh || prompt_to_exit 1
-
-# Install Google Cloud CLI
-bash ./bin/install_google_cloud_cli.sh || prompt_to_exit 1
-
-# Install Google Firebase CLI
-bash ./bin/install_google_firebase_cli.sh || prompt_to_exit 1
-
-# Install Signal Desktop
-bash ./bin/install_signal_desktop.sh || prompt_to_exit 1
-
-# Install Bitwarden
-sudo snap install bitwarden || prompt_to_exit 1
-
-# Install Telegram Desktop
-sudo snap install telegram-desktop || prompt_to_exit 1
-
-# Install Spotify
-sudo snap install spotify || prompt_to_exit 1
-
-# Install Oracle VirtualBox
-bash ./bin/install_oracle_virtualbox.sh || prompt_to_exit 1
-# Configure Oracle VirtualBox
-bash ./bin/configure_oracle_virtualbox.sh || prompt_to_exit 1
-
-# Install Docker
-bash ./bin/install_docker.sh || prompt_to_exit 1
-
-# Install Vagrant
-bash ./bin/install_vagrant.sh || prompt_to_exit 1
-# Configure Vagrant
-bash ./bin/configure_vagrant.sh || prompt_to_exit 1
-
-# TODO: Configure Mozilla Firefox
-# bash ./bin/configure_mozilla_firefox.sh || prompt_to_exit 1
-
-# Install Google Chrome
-bash ./bin/install_google_chrome.sh || prompt_to_exit 1
-# TODO: Configure Google Chrome
-# bash ./bin/configure_google_chrome.sh || prompt_to_exit 1
-
-# TODO: Install Geckdriver
-# bash ./bin/install_geckodriver.sh || prompt_to_exit 1
-
-# TODO: Install Chromedriver
-# bash ./bin/install_chromedriver.sh || prompt_to_exit 1
-
-# Configure Canonical Livepatch
-bash ./bin/configure_livepatch.sh
-
-# Configure Firewall (UFW)
-bash ./bin/configure_ufw.sh || prompt_to_exit 1
-
-# Configure SSH
-bash ./bin/configure_ssh.sh || prompt_to_exit 1
-
-# Configure Swapfile
-bash ./bin/configure_swapfile.sh "$SWAPFILE_PATH" "$SWAPFILE_SIZE" "$SWAPFILE_SWAPINESS" || prompt_to_exit 1
-
-# Configure dconf
-bash ./bin/configure_dconf.sh || prompt_to_exit 1
-
-# Change power mode to "performance"
-powerprofilesctl set performance || prompt_to_exit 1
-
-# Change processor governors to "Performance"
+# TODO: Could change processor governors to "Performance"
 # https://askubuntu.com/questions/604720/setting-to-high-performance
 # View available:
 #   cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors
@@ -237,16 +121,59 @@ powerprofilesctl set performance || prompt_to_exit 1
 #   cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 # Change setting:
 #   echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+# Register tasks
 
-# Configure bluetooth
-bash ./bin/configure_bluetooth.sh || prompt_to_exit 1
+register_task 'DNS'                 configure 'bash ./bin/configure_dns.sh' 1
+register_task 'Firewall (UFW)'      configure 'bash ./bin/configure_ufw.sh' 1
+register_task 'SSH'                 configure 'bash ./bin/configure_ssh.sh' 1
+register_task 'Canonical Livepatch' configure "bash ./bin/configure_livepatch.sh '$LIVEPATCH_KEY'" 0
 
-# TODO: Install Anaconda
-# bash ./bin/install_anaconda.sh || prompt_to_exit 1
-
-# TODO: Install NVIDIA CUDA 11.6
+register_task 'Dependencies'        install   'bash ./bin/install_dependencies.sh' 1
+register_task 'General Packages'    install   'bash ./bin/install_general.sh' 1
+register_task 'Bluetooth'           configure 'bash ./bin/configure_bluetooth.sh' 1
 # (check comment in install script for 11.3)
-# bash ./bin/install_nvidia_cuda.sh || prompt_to_exit 1
+# register_task 'NVIDIA CUDA'         install 'bash ./bin/install_nvidia_cuda.sh' 1
+
+register_task 'Swapfile'            configure "bash ./bin/configure_swapfile.sh '$SWAPFILE_PATH' '$SWAPFILE_SIZE' '$SWAPFILE_SWAPINESS'" 1
+register_task 'dconf'               configure 'bash ./bin/configure_dconf.sh' 1
+register_task 'Power Mode'          configure 'powerprofilesctl set performance' 1
+
+register_task 'HTop'                configure 'bash ./bin/configure_htop.sh' 0
+register_task 'Python'              install   'bash ./bin/install_python3.sh' 1
+register_task 'Python'              configure 'bash ./bin/configure_python3.sh' 1
+# register_task 'Python 3.9'          install   'bash ./bin/install_python39.sh' 1
+# register_task 'Python 3.9'          configure 'bash ./bin/configure_python39.sh' 1
+#       ~/.cmdstan/cmdstan-2.29.1
+# register_task 'CmdStan'             install 'bash ./bin/install_cmdstan.sh' 1
+#       ~/.mujoco/mujoco210
+# register_task 'MuJoCo'              install 'bash ./bin/install_mujoco.sh' 1
+#       ~/.streamlit/config.toml
+# register_task 'Streamlit'           configure 'bash ./bin/configure_streamlit.sh' 1
+register_task 'Node.js'             install   'bash ./bin/install_nodejs.sh' 1
+register_task 'Yarn'                install   'bash ./bin/install_yarn.sh' 1
+register_task 'FiraCode Font'       install   'bash ./bin/install_firacode_font.sh' 1
+register_task 'Sublime Text'        install   'bash ./bin/install_sublime_text.sh' 1
+register_task 'Visual Studio Code'  install   'bash ./bin/install_visual_studio_code.sh' 1
+register_task 'Google Cloud CLI'    install   'bash ./bin/install_google_cloud_cli.sh' 1
+register_task 'Google Firebase CLI' install   'bash ./bin/install_google_firebase_cli.sh' 1
+register_task 'Signal Desktop'      install   'bash ./bin/install_signal_desktop.sh' 1
+register_task 'Bitwarden'           install   'sudo snap install bitwarden' 1
+register_task 'Telegram Desktop'    install   'sudo snap install telegram-desktop' 1
+register_task 'Spotify'             install   'sudo snap install spotify' 1
+register_task 'Oracle VirtualBox'   install   'bash ./bin/install_oracle_virtualbox.sh' 1
+register_task 'Oracle VirtualBox'   configure 'bash ./bin/configure_oracle_virtualbox.sh' 1
+register_task 'Docker'              install   'bash ./bin/install_docker.sh' 1
+register_task 'Vagrant'             install   'bash ./bin/install_vagrant.sh' 1
+register_task 'Vagrant'             configure 'bash ./bin/configure_vagrant.sh' 1
+# register_task 'Mozilla Firefox'     configure 'bash ./bin/configure_mozilla_firefox.sh' 1
+register_task 'Google Chrome'                                           install 'bash ./bin/install_google_chrome.sh' 1
+# register_task 'Google Chrome'       configure 'bash ./bin/configure_google_chrome.sh' 1
+# register_task 'Geckodriver'         install 'bash ./bin/install_geckodriver.sh' 1
+# register_task 'Chromedriver'        install 'bash ./bin/install_chromedriver.sh' 1
+# register_task 'Anaconda'            install 'bash ./bin/install_anaconda.sh' 1
+
+# Execute tasks
+execute_tasks
 
 # Uninstall packages that are no longer dependencies (previously installed automatically)
 sudo apt-get autoremove -y
@@ -265,3 +192,27 @@ echo '  To configure GCloud, run '"'"'gcloud init'"'"' in a new window'
 echo '  To update GCloud, run '"'"'gcloud components update'"'"' in a new window'
 
 exit 0
+
+
+
+# function testit () {
+#     local a="$1"
+#     local b="$2"
+#     echo 'THIS IS THE TEST'
+#     echo $a
+#     echo $b
+# }
+
+# reset_tasks
+
+# param_a='value A with spaces'
+# param_b='value B with spaces'
+# echo 'Param A: '"$param_a"'  Param B: '"$param_b"
+
+# echo 'Testing function to register'
+# testit "$param_a" "$param_b"
+
+# echo 'Try registering and running task'
+# register_task 'HTop' 'install' "testit '$param_a' '$param_b'"
+# execute_tasks
+# ```
