efae47f0150f0fd9dadc8fa40012dab394305676
yaphott <londowski@proton.me>
2025-07-12 15:26:06 -0500

    feat(install_nvm): install NVM from latest GitHub release
    * Version is no longer hardcoded.
    * Using install script in the release instead of generalized install script.

--- bin/install_nvm.sh
+++ bin/install_nvm.sh
@@ -1,14 +1,32 @@
 #!/bin/bash -e
 
+tmp_dir="$(mktemp -d)"
+
 echo '+++ Installing Node Version Manager'
 
+# Download the latest version
+repo_owner='nvm-sh'
+repo_name='nvm'
+latest_tag="$(curl -fsLS --proto '=https' --tlsv1.2 "https://api.github.com/repos/${repo_owner}/${repo_name}/releases/latest" | jq -r '.tag_name')"
+latest_url="https://github.com/${repo_owner}/${repo_name}/archive/refs/tags/${latest_tag}.tar.gz"
+latest_file_name="$(basename "$latest_url")"
+
+echo "Downloading ${latest_url}..."
+curl -fsLS --proto '=https' --tlsv1.2 "$latest_url" -o "${tmp_dir}/${latest_file_name}"
+
+echo "Extracting ${latest_file_name}..."
+tar -xzf "${tmp_dir}/${latest_file_name}" -C "${tmp_dir}"
+
 # Install
-curl -fsL --proto '=https' --tlsv1.2 -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+"${tmp_dir}/${repo_name}-${latest_tag#v}/install.sh" 
 
 # Verify installation
 export NVM_DIR="$HOME/.nvm"
 source "$NVM_DIR/nvm.sh"
 source "$NVM_DIR/bash_completion"
 nvm --version > /dev/null
+
+# Clean up
+rm -r "$tmp_dir"
 
 echo 'Node Version Manager installed successfully.'
