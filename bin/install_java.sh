c68a00e42147dfed09bfa0e7e16b1adfaa89e0bb
yaphott <londowski@proton.me>
2025-07-12 13:30:41 -0500

    fix(install_java): identify latest java version and improve verification step

--- bin/install_java.sh
+++ bin/install_java.sh
@@ -8,9 +8,18 @@ if [[ ! -x "$(command -v sdk)" ]]; then
 fi
 
 # Install latest Zulu version
-sdk install java zulu
+latest_version_full=$(sdk list java | awk '$8 ~ /^[0-9]+\.[0-9]+\.[0-9]+-zulu$/ { print $8 }' | sort -V | tail -n 1)
+if [[ -z "$latest_version_full" ]]; then
+    echo 'No Zulu Java versions found.'
+    exit 1
+fi
+sdk install java "$latest_version_full"
+sdk default java "$latest_version_full"
 
 # Verify installation
-java --version > /dev/null
+if ! sdk current java | grep -q " ${latest_version_full}$"; then
+    echo 'Failed to set default Java version.'
+    exit 1
+fi
 
 echo 'Java installed successfully.'
