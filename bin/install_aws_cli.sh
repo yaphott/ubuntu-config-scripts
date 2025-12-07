b67e2edb26f086941006d9f2e63d6d383becb55d
yaphott <londowski@proton.me>
2025-12-07 02:02:13 -0600

    refactor(install_aws_cli): correct variable name for installer file in download and extraction steps

--- bin/install_aws_cli.sh
+++ bin/install_aws_cli.sh
@@ -9,16 +9,16 @@ installer_url="https://awscli.amazonaws.com/$installer_file_name"
 
 # Download
 echo "Downloading ${installer_file_name}..."
-curl -fsLS --proto '=https' --tlsv1.2 "$installer_file_name" -o "${tmp_dir}/${linstaller_file_name}"
+curl -fsLS --proto '=https' --tlsv1.2 "$installer_file_name" -o "${tmp_dir}/${installer_file_name}"
 
 # Verify download
-if [[ ! -f "${tmp_dir}/${linstaller_file_name}" ]]; then
+if [[ ! -f "${tmp_dir}/${installer_file_name}" ]]; then
     echo "Failed to download ${installer_file_name} from ${installer_url}."
     exit 1
 fi
 
 # Extract
-unzip -q "${tmp_dir}/${linstaller_file_name}" -d "$tmp_dir"
+unzip -q "${tmp_dir}/${installer_file_name}" -d "$tmp_dir"
 
 # Install
 sudo "${tmp_dir}/aws/install"
