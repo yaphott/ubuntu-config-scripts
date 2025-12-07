7c273930aaa6b3968cc13bc6e2ddb665caba98df
yaphott <londowski@proton.me>
2025-12-07 10:47:09 -0600

    fix(configure_conda): ensure Conda is added to PATH if not already present

--- bin/configure_conda.sh
+++ bin/configure_conda.sh
@@ -2,6 +2,12 @@
 
 echo '~~~ Configuring Conda'
 
+
+# Add to path if not already
+if [[ ! -x "$(command -v conda)" ]]; then
+    export PATH="$PATH:$HOME/miniconda3/bin"
+fi
+
 conda config --add channels conda-forge
 conda config --set channel_priority strict
 
